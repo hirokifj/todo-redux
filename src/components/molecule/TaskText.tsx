88982b0650bbd15b063f4f376068efc080270eed
hirokifj <hf.rowkey313@gmail.com>
2020-12-23 23:08:33 +0900

    アニメーションを簡略化

--- src/components/molecule/TaskText.tsx
+++ src/components/molecule/TaskText.tsx
@@ -9,29 +9,11 @@ const TaskText: FC<{
 }> = ({ text, isChecked }) => {
   const textStyle = css`
     display: inline-block;
-    transform: translate3d(0, 0, 0);
-
-    ::before {
-      position: absolute;
-      top: 15px;
-      left: 0;
-      width: 100%;
-      height: 1px;
-      content: '';
-      background: #bbbab9;
-      transform: scaleX(0);
-      transform-origin: 0 0;
-    }
   `
 
   const checkedTextStyle = css`
     color: #bbbab9;
-    transition: all 0.3s ease;
-
-    ::before {
-      transition: all 0.3s ease;
-      transform: scaleX(1);
-    }
+    text-decoration: line-through;
   `
 
   return <span css={[textStyle, isChecked && checkedTextStyle]}>{text}</span>
